e9d8471bd443724423f2be2f45e0a7cfe3331116
Cristina <cristina@forecast.it>
2021-04-04 21:29:06 +0200

    rest of the models

--- node/models/tasks.js
+++ node/models/tasks.js
@@ -1,79 +1,78 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('tasks', {
+const DataTypes = require("sequelize");
+
+const sequelize = require("../util/database");
+
+module.exports = sequelize.define(
+  "tasks",
+  {
     id: {
       autoIncrement: true,
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey: true
+      primaryKey: true,
     },
     project_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'projects',
-        key: 'id'
-      }
+        model: "projects",
+        key: "id",
+      },
     },
     task_status_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'task_statuses',
-        key: 'id'
-      }
+        model: "task_statuses",
+        key: "id",
+      },
     },
     name: {
       type: DataTypes.STRING(255),
-      allowNull: false
+      allowNull: false,
     },
     description: {
       type: DataTypes.TEXT,
-      allowNull: true
+      allowNull: true,
     },
     minutes_estimated: {
       type: DataTypes.INTEGER,
-      allowNull: true
+      allowNull: true,
     },
     start_date: {
       type: DataTypes.DATEONLY,
-      allowNull: true
+      allowNull: true,
     },
     end_date: {
       type: DataTypes.DATEONLY,
-      allowNull: true
+      allowNull: true,
     },
     locked_date: {
       type: DataTypes.DATEONLY,
-      allowNull: true
-    }
-  }, {
+      allowNull: true,
+    },
+  },
+  {
     sequelize,
-    tableName: 'tasks',
+    tableName: "tasks",
     timestamps: false,
     indexes: [
       {
         name: "PRIMARY",
         unique: true,
         using: "BTREE",
-        fields: [
-          { name: "id" },
-        ]
+        fields: [{ name: "id" }],
       },
       {
         name: "FKTasks528675",
         using: "BTREE",
-        fields: [
-          { name: "project_id" },
-        ]
+        fields: [{ name: "project_id" }],
       },
       {
         name: "FKTasks111043",
         using: "BTREE",
-        fields: [
-          { name: "task_status_id" },
-        ]
+        fields: [{ name: "task_status_id" }],
       },
-    ]
-  });
-};
+    ],
+  }
+);
--- node/models/time_registrations.js
+++ node/models/time_registrations.js
@@ -1,71 +1,70 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('time_registrations', {
+const DataTypes = require("sequelize");
+
+const sequelize = require("../util/database");
+
+module.exports = sequelize.define(
+  "time_registrations",
+  {
     id: {
       autoIncrement: true,
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey: true
+      primaryKey: true,
     },
     task_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'tasks',
-        key: 'id'
-      }
+        model: "tasks",
+        key: "id",
+      },
     },
     person_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'persons',
-        key: 'id'
-      }
+        model: "persons",
+        key: "id",
+      },
     },
     minutes_registered: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
     },
     notes: {
       type: DataTypes.TEXT,
-      allowNull: true
+      allowNull: true,
     },
     time_reg_date: {
       type: DataTypes.DATEONLY,
-      allowNull: false
+      allowNull: false,
     },
     locked: {
       type: DataTypes.BOOLEAN,
-      allowNull: true
-    }
-  }, {
+      allowNull: true,
+    },
+  },
+  {
     sequelize,
-    tableName: 'time_registrations',
+    tableName: "time_registrations",
     timestamps: false,
     indexes: [
       {
         name: "PRIMARY",
         unique: true,
         using: "BTREE",
-        fields: [
-          { name: "id" },
-        ]
+        fields: [{ name: "id" }],
       },
       {
         name: "FKTime_Regis927685",
         using: "BTREE",
-        fields: [
-          { name: "task_id" },
-        ]
+        fields: [{ name: "task_id" }],
       },
       {
         name: "FKTime_Regis835468",
         using: "BTREE",
-        fields: [
-          { name: "person_id" },
-        ]
+        fields: [{ name: "person_id" }],
       },
-    ]
-  });
-};
+    ],
+  }
+);
