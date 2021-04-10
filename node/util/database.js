c268ce657afb39f12fbf0ade704314bae55da8dc
Cristina <cristina@forecast.it>
2021-04-10 15:16:10 +0200

    remove file from the repo, but not from the file system

--- node/util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("management", "root", "louisiana", {
-  dialect: "mysql",
-  host: "localhost",
-});
-
-module.exports = sequelize;
